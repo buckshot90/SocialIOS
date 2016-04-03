dcf73a559b7e4763637e62844f914d35efe746b8
Rusinov Vitaliy <rvp90@mail.ru>
2016-04-03 10:22:22 +0300

    Start implementation of business logic.

---
+++ Source/BusinessLogicLayer/Services/ConcreteServices/FriendService/SOLFriendService.m
@@ -0,0 +1,13 @@
+//
+//  SOLFriendService.m
+//  OutputFaceBookData
+//
+//  Created by Vitaliy Rusinov on 4/3/16.
+//  Copyright © 2016 Oleh Petrunko. All rights reserved.
+//
+
+#import "SOLFriendService.h"
+
+@implementation SOLFriendService
+
+@end
---
+++ Source/BusinessLogicLayer/Services/ConcreteServices/UserService/SOLUserService.m
@@ -0,0 +1,13 @@
+//
+//  SOLUserService.m
+//  OutputFaceBookData
+//
+//  Created by Vitaliy Rusinov on 4/3/16.
+//  Copyright © 2016 Oleh Petrunko. All rights reserved.
+//
+
+#import "SOLUserService.h"
+
+@implementation SOLUserService
+
+@end
